19260ae9e53183989bfb1559f8b983e80c40a7a3
YandexMapKit <robot-mapkit-ci@yandex-team.ru>
2026-06-26 17:39:16 +0300

    Release 4.39.1-beta

---
+++ lib/src/runtime/auth/account_export.dart
@@ -0,0 +1,6 @@
+export 'package:yandex_maps_mapkit/src/runtime/auth/account.dart'
+    hide
+        AccountImpl,
+        TokenListenerImpl,
+        TokenListenerContainerExtension,
+        PasswordRequiredDataImpl;
